cced6e1a5872cd768003782f6c49af1410042e52
MarynaDanilkina <Danilkinamari99@gmail.com>
2022-02-22 20:10:02 +0300

    feat: completed the task count cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  if (matrix.length === 0) {
+    return 0
+  }
+  let result = 0;
+  for (i = 0; i < matrix.length; i++) {
+    console.log(matrix[i])
+    result += matrix[i].filter(el => el == '^^').length
+  }
+  return result
+
 }
 
 module.exports = {
