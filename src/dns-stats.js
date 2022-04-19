3db315bb6987bfda690c5bc9674bea40e39cca80
MarynaDanilkina <Danilkinamari99@gmail.com>
2022-04-19 22:31:24 +0300

    feat: dns stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,11 +22,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function getDNSStats(domains) {
+  let obj = {};
+  for (let i = 0; i < domains.length; i++) {
+    let arrDomains = domains[i].split(".")
+    let reversDomains = arrDomains.reverse();
+    console.log(reversDomains)
+    for (let j = 0; j < reversDomains.length; j++) {
+      let domain = `.${reversDomains.slice(0, j + 1).join('.')}`
+      obj[domain] = obj[domain] ? obj[domain] + 1 : 1
 
+    }
+  }
+  return obj
+}
+getDNSStats(['epam.com', 'info.epam.com'])
 module.exports = {
   getDNSStats
 };
