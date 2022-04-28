19a0d9407daec77667705082c13b589aeac957e3
MarynaDanilkina <Danilkinamari99@gmail.com>
2022-04-28 19:48:43 +0300

    feat: add vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,58 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+    message = message.toUpperCase()
+    key = key.toUpperCase()
+    const alfa = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let count = 0;
+    let arr = message.split("").map((el, index) => {
+      if (!alfa.includes(el)) {
+        return el;
+      }
+      let codeMessage = el.charCodeAt()
+      let codeKey = key.charCodeAt(count % key.length)
+      let codeResult = (codeMessage - 65 + codeKey - 65) % 26 + 65
+      let result = String.fromCharCode(codeResult);
+      count++;
+      return result
+    })
+    if (this.isDirect) {
+      return arr.join('')
+    } else {
+      return arr.reverse().join('')
+    }
+  }
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+    encryptedMessage = encryptedMessage.toUpperCase()
+    key = key.toUpperCase()
+    const alfa = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let count = 0;
+    let arr = encryptedMessage.split("").map((el, index) => {
+      if (!alfa.includes(el)) {
+        return el;
+      }
+      let codeMessage = el.charCodeAt()
+      let codeKey = key.charCodeAt(count % key.length)
+      let codeResult = ((codeMessage - codeKey + 26) % 26)
+      let result = String.fromCharCode(codeResult + 65);
+      count++;
+      return result
+    })
+    if (this.isDirect) {
+      return arr.join('')
+    } else {
+      return arr.reverse().join('')
+    }
   }
 }
 
